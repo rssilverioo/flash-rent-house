b6ee45c0a3d33a4c708b98f0e9aad02ff4d7107c
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2023-11-27 20:50:04 -0300

    Individual house listing

--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -2,8 +2,8 @@
 
 
 import useCountries from '@/app/hooks/useCountries';
-import { SafeUser } from '@/app/types';
-import { Listing, Reservation } from '@prisma/client';
+import { SafeListings, SafeUser } from '@/app/types';
+import {  Reservation } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
 import Image from 'next/image';
@@ -13,7 +13,7 @@ import HeartButton from '../HeartButton';
 import Button from '../Button';
 
 interface ListingCardProps {
-	data: Listing;
+	data: SafeListings;
 	reservation?: Reservation;
 	onAction?: (id: string) => void;
 	disabled?: boolean;
@@ -68,7 +68,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
 	}, [reservation]);
 
 	return (
-		<div onClick={() => router.push(`/listing/${data.id}`)} className='col-span-1 cursor-pointer group'>
+		<div onClick={() => router.push(`/listings/${data.id}`)} className='col-span-1 cursor-pointer group'>
 			<div className='flex flex-col gap-2 w-full'>
 				<div className='aspect-square w-full relative overflow-hidden rounded-xl'>
 					<Image
---
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import axios from 'axios';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { differenceInDays, eachDayOfInterval } from 'date-fns';
+
+import useLoginModal from '@/app/hooks/useLoginModal';
+
+import Container from '@/app/components/Container';
+import { categories } from '@/app/components/navbar/Categories';
+import ListingHead from '@/app/components/listings/ListingHead';
+import { SafeListings, SafeUser } from '@/app/types';
+import { Reservation } from '@prisma/client';
+import ListingInfo from '@/app/components/listings/ListingInfo';
+
+
+const initialDateRange = {
+	startDate: new Date(),
+	endDate: new Date(),
+	key: 'selection'
+};
+
+interface ListingClientProps {
+  reservations?: Reservation[];
+  listing: SafeListings & {
+    user: SafeUser;
+  };
+  currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+	listing,
+	currentUser
+}) => {
+
+
+
+	const category = useMemo(() => {
+		return categories.find((items) =>
+			items.label === listing.category);
+	}, [listing.category]);
+
+
+
+	return (
+		<Container>
+			<div
+				className="
+          max-w-screen-lg
+          mx-auto
+        "
+			>
+				<div className="flex flex-col gap-6">
+					<ListingHead
+						title={listing.title}
+						imageSrc={listing.imageSrc}
+						locationValue={listing.locationValue}
+						id={listing.id}
+						currentUser={currentUser}
+					/>
+					<div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+						<ListingInfo
+							user={listing.user}
+							category={category}
+							description={listing.description}
+							roomCount={listing.roomCount}
+							guestCount={listing.guestCount}
+							bathroomCount={listing.bathroomCount}
+							locationValue={listing.locationValue}
+						/>
+					</div>
+
+				</div>
+			</div>
+		</Container>
+	);
+};
+
+export default ListingClient;
